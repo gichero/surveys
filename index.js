bc147f20c09440f284fff99d23ff79950ed38d4d
Amos Gichero <amosgichero@gmail.com>
2020-09-04 12:37:16 -0400

    resolve configuration

--- index.js
+++ index.js
@@ -35,9 +35,14 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 	//Express will serve up the index.html file
 	//if it doesn't recognise the route
-	const path = require("path");
+	// const path = require("path");
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	// });
+	const root = require("path").join(__dirname, "client", "build");
+	app.use(express.static(root));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+		res.sendFile("index.html", { root });
 	});
 }
 
